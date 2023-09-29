8f1b3c931930f2a95ab8baf146d240f68cad0f71
emeliehensfelt <emelie046@gmail.com>
2023-09-29 12:02:02 +0200

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,95 @@
+﻿namespace AbstractCalculator
+{
+
+    internal class Calculator
+    {
+        public Calculator() { }
+
+        public void Calculate(string nbr1, string nbr2, string op)
+        {
+            bool IsNumber1 = ValidateInput(nbr1);
+            bool IsNumber2 = ValidateInput(nbr2);
+            int calcResult;
+
+            if (IsNumber1 && IsNumber2)
+            {
+                int number1 = ConversionResult(nbr1);
+                int number2 = ConversionResult(nbr2);
+
+                if (op.Equals("add", StringComparison.OrdinalIgnoreCase))
+                {
+                    op = "+";
+                    calcResult = Add(number1, number2);
+                    DisplayResult(number1, number2, op, calcResult);
+
+                }
+                else if (op.Equals("multiply", StringComparison.OrdinalIgnoreCase))
+                {
+                    op = "x";
+                    calcResult = Multiply(number1, number2);
+                    DisplayResult(number1, number2, op, calcResult);
+                }
+                else if (op.Equals("divide", StringComparison.OrdinalIgnoreCase))
+                {
+                    op = "/";
+                    calcResult = Divide(number1, number2);
+                    DisplayResult(number1, number2, op, calcResult);
+                }
+                else if (op.Equals("subtract", StringComparison.OrdinalIgnoreCase))
+                {
+                    op = "-";
+                    calcResult = Subtract(number1, number2);
+                    DisplayResult(number1, number2, op, calcResult);
+                }
+
+            }
+            else if (!IsNumber1 || !IsNumber2)
+            {
+                Console.WriteLine("Conversion failed, please input two numbers.");
+            }
+
+        }
+
+        private void DisplayResult(int a, int b, string op, int sum)
+        {
+            string display = ($"{a} {op} {b} = {sum}");
+            Console.WriteLine(display);
+        }
+
+
+        private int Add(int nbr1, int nbr2)
+        {
+            int sum = nbr1 + nbr2;
+            return sum;
+        }
+        private int Subtract(int nbr1, int nbr2)
+        {
+            int sum = nbr1 - nbr2;
+            return sum;
+        }
+
+        private int Divide(int nbr1, int nbr2)
+        {
+            int sum = nbr1 / nbr2;
+            return sum;
+        }
+        private int Multiply(int nbr1, int nbr2)
+        {
+            int sum = nbr1 * nbr2;
+            return sum;
+        }
+        private bool ValidateInput(string input)
+        {
+            int int_conversion;
+            bool result = int.TryParse(input, out int_conversion);
+            return result;
+        }
+
+        private int ConversionResult(string number)
+        {
+            int n = int.Parse(number);
+            return n;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+using AbstractCalculator;
+Calculator calculator = new Calculator();
+bool isRunning = true;
+
+while (isRunning)
+{
+    Console.Clear();
+    Header();
+    Console.WriteLine("Type the first number: ");
+    Console.Write("First number: ");
+    string input1 = Console.ReadLine();
+
+    Console.WriteLine("Type the second number: ");
+    Console.Write("Second number: ");
+    string input2 = Console.ReadLine();
+
+    Console.WriteLine("Which type of calculation do you wish to do?\n type 'add', 'subtract', 'divide' or 'multiply'");
+    string op = Console.ReadLine();
+    calculator.Calculate(input1, input2, op);
+
+    Console.WriteLine("Do you want to make a new calculation? type 'y' for yes and 'n' for no.");
+    string answer = Console.ReadLine();
+
+    if (answer.Equals("n") || answer.Equals("N"))
+    {
+        Console.Clear();
+        Finished();
+        isRunning = false;
+    }
+
+}
+
+
+void Header()
+{
+    Console.WriteLine("  .--.      .-'.      .--.      .--.      .--.      .--.      .`-.      .--.\r\n:::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\\r\n'      `--'      `.-'      `--'      `--'      `--'      `-.'      `--'      `");
+    Console.WriteLine("       SUPER COOL ABSTRACT CALCULATOR          ");
+    Console.WriteLine("  .--.      .-'.      .--.      .--.      .--.      .--.      .`-.      .--.\r\n:::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\\r\n'      `--'      `.-'      `--'      `--'      `--'      `-.'      `--'      `");
+}
+
+void Finished()
+{
+    Console.WriteLine(" _____ ___ _   _ ___ ____  _   _ _____ ____   \r\n|  ___|_ _| \\ | |_ _/ ___|| | | | ____|  _ \\  \r\n| |_   | ||  \\| || |\\___ \\| |_| |  _| | | | | \r\n|  _|  | || |\\  || | ___) |  _  | |___| |_| | \r\n|_|   |___|_| \\_|___|____/|_| |_|_____|____/  ");
+}
